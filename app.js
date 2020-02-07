7a9aceb102168fcdc1c41be8bf5f6a17fb0b06e6
Jane Chuk <humhum@live.com>
2020-02-07 14:25:42 -0500

    fix- dynamic li issues

--- app.js
+++ app.js
@@ -1,51 +1,71 @@
-
-let ul = document.querySelector("#lists")
-let form = document.querySelector("form")
-let input = document.querySelector('input[name="newTodo"]')
-let toggleForm = document.querySelector("#toggle-form")
-let spans = document.querySelectorAll("span")
+document.addEventListener("DOMContentLoaded", function(){
 
 
-//function to delete todo if delete span is clicked.
-function deleteTodo(){
-    for(let span of spans){
-      span.addEventListener ("click",function (){
-        span.parentElement.remove();
-        event.stopPropagation();
-      });
-    }
-  }
+  let ul = document.querySelector("#lists")
+  let form = document.querySelector("form")
+  let input = document.querySelector('input[name="newTodo"]')
+  let toggleForm = document.querySelector("#toggle-form")
+  let spans = document.querySelectorAll("span")
+  
+  
+  
+  // this function creats a new list when u hit submitt
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+  let li = document.createElement("li")
+  let icon = document.createElement("i")   
+  let newTodo = input.value;
+  input.value = " " ;
     
-    // event listener to linethrough list if clicked
-    ul.addEventListener("click", function(e){
-     if (e.target.tagName === "LI"){
-        e.target.classList.toggle("completed")
-        
-       }}
-    )
+  icon.classList.add('fas', 'fa-trash-alt');
+  li.append(icon);
+  ul.appendChild(li).append(newTodo);
 
-// this function creats a new list when u hit submitt
-    form.addEventListener("submit", function(e){
-        e.preventDefault();
-    let li = document.createElement("li")
-    let spanElement = document.createElement("span")
-    let icon = document.createElement("i")   
-    let newTodo = input.value;
-    input.value = " " ;
-        
-    icon.classList.add('fas', 'fa-trash-alt');
-    spanElement.append(icon);
-    ul.appendChild(li).append(spanElement,newTodo);
+
+  deleteTodo();
+  })
+  
+  
+  // // alternate function to delete todo if delete span is clicked.
+  // function deleteTodo(){
+  //     for(let span of spans){
+  //       span.addEventListener ("click",function (){
+  //         span.parentElement.remove();
+  //         event.stopPropagation();
+  //       });
+  //     }}
+
+
+  
+  //function to delete todo if delete span is clicked.
+  ul.addEventListener("click", function(e){
+    if (e.target.tagName === "I"){
+       e.target.parentElement.remove();
+      }}
+   )
+
+
       
-    deleteTodo();
-    })
-    
-    //hides the input form,when pencil icon is clicked
-    toggleForm.addEventListener("click", function(e){
-        form.classList.toggle("toggleform")
-    })
+      // event listener to linethrough list if clicked
+      ul.addEventListener("click", function(e){
+       if (e.target.tagName === "LI"){
+          e.target.classList.toggle("completed")
+          
+         }}
+      )
+  
+  
+      
+      //hides the input form,when pencil icon is clicked
+      toggleForm.addEventListener("click", function(e){
+          form.classList.toggle("toggleform")
+      })
+  
+  //delete todo
+  deleteTodo();
+  
+  
+  
 
-//delete todo
-deleteTodo();
-
+})
 
