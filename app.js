8c3ca98f6d08d73df5626a293ae37a473b728d75
Jane Chuk <humhum@live.com>
2020-02-07 16:01:54 -0500

    Add save/clear btns/localstorage

--- app.js
+++ app.js
@@ -1,71 +1,78 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener('DOMContentLoaded', function() {
+	let ul = document.querySelector('#lists');
+	let form = document.querySelector('form');
+	let input = document.querySelector('input[name="newTodo"]');
+	let toggleForm = document.querySelector('#toggle-form');
+	let spans = document.querySelectorAll('span');
+	let saveBtn = document.querySelector('.save');
+	let clearBtn = document.querySelector('.clear');
 
+	// this function creats a new list when u hit submitt
+	form.addEventListener('submit', function(e) {
+		e.preventDefault();
+		let li = document.createElement('li');
+		let icon = document.createElement('i');
+		let newTodo = input.value;
+		input.value = ' ';
 
-  let ul = document.querySelector("#lists")
-  let form = document.querySelector("form")
-  let input = document.querySelector('input[name="newTodo"]')
-  let toggleForm = document.querySelector("#toggle-form")
-  let spans = document.querySelectorAll("span")
-  
-  
-  
-  // this function creats a new list when u hit submitt
-  form.addEventListener("submit", function(e){
-    e.preventDefault();
-  let li = document.createElement("li")
-  let icon = document.createElement("i")   
-  let newTodo = input.value;
-  input.value = " " ;
-    
-  icon.classList.add('fas', 'fa-trash-alt');
-  li.append(icon);
-  ul.appendChild(li).append(newTodo);
+		icon.classList.add('fas', 'fa-trash-alt');
+		li.append(icon);
+		ul.appendChild(li).append(newTodo);
 
+		deleteTodo();
+	});
 
+	// // alternate function to delete todo if delete span is clicked.
+	// function deleteTodo(){
+	//     for(let span of spans){
+	//       span.addEventListener ("click",function (){
+	//         span.parentElement.remove();
+	//         event.stopPropagation();
+	//       });
+	//     }}
+
+	//function to delete todo if delete span is clicked.
+	ul.addEventListener('click', function(e) {
+		if (e.target.tagName === 'I') {
+			e.target.parentElement.remove();
+		}
+	});
+
+	// event listener to linethrough list if clicked
+	ul.addEventListener('click', function(e) {
+		if (e.target.tagName === 'LI') {
+			e.target.classList.toggle('completed');
+		}
+	});
+
+	//hides the input form,when pencil icon is clicked
+	toggleForm.addEventListener('click', function(e) {
+		form.classList.toggle('toggleform');
+	});
+
+	//function to load todo if list is found in local storage.
+	function loadTodo() {
+		if (localStorage.getItem('todoList')) {
+			ul.innerHTML = localStorage.getItem('todoList');
+			deleteTodo();
+		}
+	}
+
+	//save todolist state so user can access it later
+	saveBtn.addEventListener('click', function() {
+		localStorage.setItem('todoList', ul.innerHTML);
+	});
+
+	//clear all todo when clear button is clicked
+	clearBtn.addEventListener('click', function() {
+		ul.innerHTML = '';
+		localStorage.removeItem('todoList', ul.innerHTML);
+	});
+
+	//delete todo
   deleteTodo();
-  })
   
-  
-  // // alternate function to delete todo if delete span is clicked.
-  // function deleteTodo(){
-  //     for(let span of spans){
-  //       span.addEventListener ("click",function (){
-  //         span.parentElement.remove();
-  //         event.stopPropagation();
-  //       });
-  //     }}
+//load Todo
+loadTodo();
 
-
-  
-  //function to delete todo if delete span is clicked.
-  ul.addEventListener("click", function(e){
-    if (e.target.tagName === "I"){
-       e.target.parentElement.remove();
-      }}
-   )
-
-
-      
-      // event listener to linethrough list if clicked
-      ul.addEventListener("click", function(e){
-       if (e.target.tagName === "LI"){
-          e.target.classList.toggle("completed")
-          
-         }}
-      )
-  
-  
-      
-      //hides the input form,when pencil icon is clicked
-      toggleForm.addEventListener("click", function(e){
-          form.classList.toggle("toggleform")
-      })
-  
-  //delete todo
-  deleteTodo();
-  
-  
-  
-
-})
-
+});
